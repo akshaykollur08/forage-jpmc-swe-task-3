36c4c50476bfc51fc26df963dbe2d7eef7d2ea71
Akshay Kollur <akshaykollur08@gmail.com>
2024-06-21 13:39:05 -0500

    Task 3 completed

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,47 @@
 import { ServerRespond } from './DataStreamer';
 
-export interface Row {
-  stock: string,
-  top_ask_price: number,
+export interface Row
+{
+  // This is needed to determine the structure of the object returned by the generateRow method
+  price_abc: number,
+  price_def: number,
+  ratio: number,
   timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined
+
+
+
+
+
+
 }
 
 
-export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+export class DataManipulator
+{
+  static generateRow(serverRespond: ServerRespond[]): Row
+  {
+    //Creation of price constants for the ticker ABC and DEF stock
+    const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2;
+    const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2;
+
+    //Creation of the ratio constant that is simply ABC price over DEF price
+    const ratio = priceABC / priceDEF;
+    // The threshold is +-10% of the 12 month historical average ratio
+    const upperBound = 1 + 0.1;
+    const lowerBound = 1 - 0.1;
+
+    return {
+      price_abc: priceABC,
+      price_def: priceDEF,
+      ratio,
+      timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ? serverRespond[0].timestamp : serverRespond[1].timestamp,
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+      trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
+    };
+
   }
 }
--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,58 +1,89 @@
 import React, { Component } from 'react';
-import { Table } from '@finos/perspective';
+import { Table, TableData } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import { DataManipulator } from './DataManipulator';
 import './Graph.css';
 
-interface IProps {
+interface IProps
+{
   data: ServerRespond[],
 }
 
-interface PerspectiveViewerElement extends HTMLElement {
+interface PerspectiveViewerElement extends HTMLElement
+{
   load: (table: Table) => void,
 }
-class Graph extends Component<IProps, {}> {
+class Graph extends Component<IProps, {}>
+{
   table: Table | undefined;
 
-  render() {
+  render()
+  {
     return React.createElement('perspective-viewer');
   }
 
-  componentDidMount() {
+  componentDidMount()
+  {
     // Get element from the DOM.
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
+      price_abc: 'float',
+      price_def: 'float',
+      ratio: 'float',
       timestamp: 'date',
+      upper_bound: 'float',
+      lower_bound: 'float',
+      trigger_alert: 'float',
     };
 
-    if (window.perspective && window.perspective.worker()) {
+    if (window.perspective && window.perspective.worker())
+    {
       this.table = window.perspective.worker().table(schema);
     }
-    if (this.table) {
+    if (this.table)
+    {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
+
+      // continuous line graph is utilized with the view setup with the y_line style graph
       elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
+
+      // column0pivots property addition to the table allows the stock to be distinguishable on the x-axis or columns set up, 
+      // not concerned with stock price anymore, just ratios, so 'column-pivots' property is removed
+      // elem.setAttribute('column-pivots', '["stock"]');
+
+      // row-pivots property addition takes care of the x-axis as timestamps
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+
+      // columns, Helps us focus on a particular part of a stock's data along the y-axis. With this we can print just the data we care about in this case, top_ask_price data for the stock      
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]');
+
+      // The aggregates property helps us to handle the cases of duplicated data, observed earlier with just simply clicking the "Start Streaming Data" button
+      // In our case, a data point is considered unique if it has a unique ticker and timestamp. Duplicate prices are averaged out.
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
+        price_abc: 'avg',
+        price_def: 'avg',
+        ratio: 'avg',
         timestamp: 'distinct count',
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg',
       }));
+
+
+
     }
   }
 
-  componentDidUpdate() {
-    if (this.table) {
-      this.table.update(
+  componentDidUpdate()
+  {
+    if (this.table) 
+    {
+      this.table.update([
         DataManipulator.generateRow(this.props.data),
-      );
+        // This is another component lifecycle method that is executed whenever the component updates or when the graph receives new data!
+      ] as unknown as TableData);
     }
   }
 }
